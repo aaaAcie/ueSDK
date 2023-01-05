7a09faa5fc36be1dda18deff367d07750dbb8be1
徐亦快 <913587892@qq.com>
2023-01-05 08:55:58 +0800

    增加项目的增删改查重构

--- src/generalOperation/project.ts
+++ src/generalOperation/project.ts
@@ -1,15 +1,21 @@
-import { operProject } from '../api/api.js'
+// import { operProject } from '../api/api.js'
 import { addResponseEventListener, emitUIInteraction} from '../basic2/myApp.js'
+import { 
+  selectProject,
+  insertProject,
+  deleteProjectById,
+  updateProjectById
+} from '../api/project.js'
 
 //  查询项目
 export async function queryProject(): Promise<{}> {
-  const { data } = await operProject({
-    "oper_type": "selectProject"
+  const { data } = await selectProject({
+    // "oper_type": "selectProject"
   })
   let Message: {}
   return new Promise<{}>((resolve, reject) => {
-    if(data.code==200){
-      Message = data.data
+    if(data.code==1001){
+      Message = data.value
       resolve({Message})
     }else{
       reject(new Error(data.msg))
@@ -20,14 +26,14 @@ export async function queryProject(): Promise<{}> {
 
 //  新增项目 数睿传来项目id，返回两个关卡id
 export async function addProject(project_id: string): Promise<{}> {
-  const { data } = await operProject({
-    "oper_type": "insertProject",
+  const { data } = await insertProject({
+    // "oper_type": "insertProject",
     project_id
   })
   let Message: {}
   return new Promise<{}>((resolve, reject) => {
-    if(data.code==200){
-      Message = data.data
+    if(data.code==1001){
+      Message = data.value
       resolve({Message})
     }else{
       reject(new Error(data.msg))
@@ -38,14 +44,14 @@ export async function addProject(project_id: string): Promise<{}> {
 
 //  删除项目
 export async function deleteProject(project_id: string): Promise<{}> {
-  const { data } = await operProject({
-    "oper_type": "deleteProject",
+  const { data } = await deleteProjectById({
+    // "oper_type": "deleteProject",
     project_id
   })
   let Message: {}
   return new Promise<{}>((resolve, reject) => {
-    if(data.code==200){
-      Message = data.data
+    if(data.code==1001){
+      Message = data.value
       resolve({Message})
     }else{
       reject(new Error(data.msg))
